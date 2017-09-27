57312c2ef84f25330d77a58f21295b66c5a876a2
Maciek <maciej.m.maslowski@gmail.com>
2017-09-27 08:05:00 +0200

    set up environment

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+
+  devServer: {
+      historyApiFallback: true
+  },
+    
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader", "eslint-loader"]}
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
