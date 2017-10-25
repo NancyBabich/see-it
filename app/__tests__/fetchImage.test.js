7fc5de55d542989ec702e4494f38761a1b551cd9
Maciek <maciej.m.maslowski@gmail.com>
2017-10-25 11:10:43 +0200

    use axios in tests to replace fetch

--- app/__tests__/fetchImage.test.js
+++ app/__tests__/fetchImage.test.js
@@ -1,21 +1,17 @@
 import expect from 'expect';
+import axios from 'axios';
+
+import axiosConfig from '../consts/axiosConfig';
 
 describe('fetchImage method in IndividualImageContainer', () => {
   it('Should fetch image with specific id', done => {
     const id = 'SLxbEMVNeqI';
     const url = `https://api.unsplash.com/photos/${id}`;
-    const request = new Request(`${url}/`, {
-      method: 'GET',
-      headers: new Headers({
-        Authorization:
-          'Client-ID e6265d8a013e80cb7c27328768e0aa508ce426d36e58106d377a5d137e421c59'
-      })
-    });
 
-    fetch(request)
-      .then(res => res.json())
-      .then(image => {
-        expect(image.id).toBe(id);
+    axios
+      .get(url, axiosConfig)
+      .then(res => {
+        expect(res.data.id).toBe(id);
       })
       .then(() => done(), done);
   });
--- app/__tests__/fetchMultipleImages.test.js
+++ app/__tests__/fetchMultipleImages.test.js
@@ -1,47 +1,36 @@
 import expect from 'expect';
+import axios from 'axios';
+
+import axiosConfig from '../consts/axiosConfig';
 
 describe('fetchImages method in ImageListContainer', () => {
-  it('Should fetch first n images', done => {
-    const url = 'https://api.unsplash.com/search/photos';
-    const category = 'cities';
-    const perPage = 9;
-    const request = new Request(
-      `${url}/?query=${category}&page=1&per_page=${perPage}`,
-      {
-        method: 'GET',
-        headers: new Headers({
-          Authorization:
-            'Client-ID e6265d8a013e80cb7c27328768e0aa508ce426d36e58106d377a5d137e421c59'
-        })
-      }
-    );
+  const root = 'https://api.unsplash.com/search/photos';
+  const perPage = 9;
 
-    fetch(request)
-      .then(res => res.json())
-      .then(images => {
-        expect(images.results.length).toBe(perPage);
+  let category;
+  let url;
+
+  it('Should fetch first n images', done => {
+    category = 'cities';
+    url = `${root}/?query=${category}&page=1&per_page=${perPage}`;
+
+    axios
+      .get(url, axiosConfig)
+      .then(res => {
+        expect(res.data.results.length).toBe(perPage);
       })
       .then(() => done(), done);
   });
 
   it('Should return no images for a non-existing category', done => {
-    const url = 'https://api.unsplash.com/search/photos';
-    const category = 'fjdjfa';
-    const perPage = 9;
-    const request = new Request(
-      `${url}/?query=${category}&page=1&per_page=${perPage}`,
-      {
-        method: 'GET',
-        headers: new Headers({
-          Authorization:
-            'Client-ID e6265d8a013e80cb7c27328768e0aa508ce426d36e58106d377a5d137e421c59'
-        })
-      }
-    );
-    fetch(request)
-      .then(res => res.json())
-      .then(images => {
-        expect(images.results.length).toBe(0);
+    category = 'kjlsdk';
+    url = `${root}/?query=${category}&page=1&per_page=${perPage}`;
+
+    axios
+      .get(url, axiosConfig)
+      .then(res => {
+        console.log(url);
+        expect(res.data.results.length).toBe(0);
       })
       .then(() => done(), done);
   });
